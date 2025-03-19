3c437e30860d9564ff88b219a28fc26afdd97635
s30213 <konradplaska@gmail.com>
2025-03-19 15:27:53 +0100

    End of lesson

--- Cwiczenie3/Cwiczenie3/Program.cs
+++ Cwiczenie3/Cwiczenie3/Program.cs
@@ -14,7 +14,7 @@ public class Container
 {
     private static int counter = 0;
     
-    public double loadWeight { get; private set; } = 0; // in kg
+    public double loadWeight { get; protected set; } = 0; // in kg
     public double maxLoadWeight { get; private set; } = 0; // in kg
     public double selfWeight { get; private set; } = 0; // in kg
     
@@ -24,6 +24,7 @@ public class Container
 
     public Container()
     {
+        
     }
 
     public Container(double loadWeight, String conType)
@@ -37,32 +38,95 @@ public class Container
         return $"KON-{conType}-{++counter}";
     }
 
-    private void Emptying()
+    public virtual void Emptying()
     {
         this.loadWeight = 0;
     }
 
-    private void Entrain()
+    public virtual void Entrain(double loadWeight)
     {
-        
+        if (loadWeight > this.maxLoadWeight)
+        {  
+            throw new OverfillException();
+        }
+        this.loadWeight = loadWeight;
     }
     
 }
 
 public class LiquidContainer : Container, IHazardNotifier
 {
-    
-    
+    public bool hazardous { get; private set; } = false;
+
+    public LiquidContainer(double loadWeight, string conType, bool hazardous) : base(loadWeight, conType)
+    {
+        this.hazardous = hazardous;
+    }
+
+    public override void Entrain(double loadWeight)
+    {
+        double tmpMaxLoadWeight = maxLoadWeight;
+        if (hazardous)
+        {
+            tmpMaxLoadWeight /= 2;
+        }
+        else
+        {
+            tmpMaxLoadWeight *= 0.9;
+        }
+
+        if (loadWeight > tmpMaxLoadWeight)
+        {
+            throw new OverfillException();
+        }
+        base.loadWeight = loadWeight;
+    }
 }
 
 public class GasContainer : Container, IHazardNotifier
 {
-    
+    public GasContainer(double loadWeight, string conType) : base(loadWeight, conType)
+    {
+        
+    }
+
+    public override void Emptying()
+    {
+        base.loadWeight = maxLoadWeight * 0.05;
+    }
 }
 
 public class FridgeContainer : Container
 {
+    public static Dictionary<string, double>  contentTemperatures { get; private set; } = new Dictionary<string, double>()
+    {
+        {"Bananas", 13.3},
+        {"Chocolate", 18.0},
+        {"Fish", 2.0},
+        {"Meat", -15.0},
+        {"Ice cream", -18.0},
+        {"Frozen pizza", -30.0},
+        {"Cheese", 7.2},
+        {"Sausages", 5},
+        {"Butter", 20.5},
+        {"Eggs", 19.0}
+    };
     
+    public double temperature { get; private set; } = 0.0;
+    public string contentType { get; private set; } = "";
+
+    public FridgeContainer(double loadWeight, string conType, double temperature, string contentType) : base(loadWeight, conType)
+    {
+        this.temperature = temperature;
+        this.contentType = contentType;
+        if (contentTemperatures[contentType] > temperature)
+        {
+            throw new Exception("Content temperature is greater than temperature");
+        }
+
+        
+
+    }
 }
 
 public interface IHazardNotifier
@@ -70,3 +134,16 @@ public interface IHazardNotifier
     
 }
 
+public class OverfillException : Exception
+{
+    public OverfillException()
+    {
+        Console.WriteLine("Masa ładunku jest większa niż pojemność kontenera");
+    }
+    public OverfillException(string message) : base(message)
+    {
+    }
+    public OverfillException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
